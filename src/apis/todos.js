e2afd825d65ff699bc199f103d52e81ca66629e8
LAVADPR <lavadpr@oocl.com>
2020-10-29 15:06:32 +0800

    [Leo&Prince] feature - change baseURL

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,17 +1,17 @@
 import api from './api'
 
 export const getTodos = () => {
-    return api.get('/api/todos');
+    return api.get('/todos');
 }
 
 export const addTodo = (text) => {
-    return api.post("/api/todos", {text: text});
+    return api.post("/todos", {text: text});
 }
 
 export const deleteTodo = (id) => {
-    return api.delete("/api/todos/" + id);
+    return api.delete("/todos/" + id);
 }
 
 export const updateTodo = (id, done) => {
-    return api.put("/api/todos/" + id, {done: !done});
+    return api.put("/todos/" + id, {done: !done});
 }
