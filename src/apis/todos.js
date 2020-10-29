382172eb2da5288595279a5771d3fa5e70dc73f7
LAVADPR <lavadpr@oocl.com>
2020-10-29 20:50:14 +0800

    [Leo&Prince] refactor - refactor export

--- src/apis/todos.js
+++ src/apis/todos.js
@@ -1,17 +1,21 @@
 import api from './api'
 
-export const getTodos = () => {
+const getTodos = () => {
     return api.get('/todos');
 }
 
-export const addTodo = (text) => {
+const addTodo = (text) => {
     return api.post("/todos", {text, done: false});
 }
 
-export const deleteTodo = (id) => {
+const deleteTodo = (id) => {
     return api.delete("/todos/" + id);
 }
 
-export const updateTodo = (id, done) => {
+const updateTodo = (id, done) => {
     return api.put("/todos/" + id, {done: !done});
+}
+
+export {
+    updateTodo, deleteTodo, addTodo, getTodos
 }
